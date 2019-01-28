277b80f03df97dbd324795f7afb1f3285c6bb091
Jake Brackney <brackney.jake@gmail.com>
2019-01-28 16:20:21 -0500

    Added Pseudocode for showing lists and creating new items

--- routes/affirmation.js
+++ routes/affirmation.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const affirmationController = require('../controllers/affirmation')
 
 router.get('/affirmation', affirmationController.index)
+//add routes & controllers for showing list and creating new
 
 module.exports = router
--- routes/gratitude.js
+++ routes/gratitude.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const gratitudeController = require('../controllers/gratitude')
 
 router.get('/gratitude', gratitudeController.index)
+//add routes & controllers for showing list and creating new
 
 module.exports = router
